ad124757fe2a82993e1105bb685e3bfe624d1b7f
zina31 <mostafa@white-code.co.uk>
2019-10-30 06:16:35 +0200

    new fields in partner

--- passap_purchase_ct/models/res_partner.py
+++ passap_purchase_ct/models/res_partner.py
@@ -4,9 +4,9 @@ from odoo import fields, models, api
 class ResPartnerChange(models.Model) :
     _inherit = 'res.partner'
 
-    commercial_register = fields.Char('Commercial Register')
-    industrial_register = fields.Char('Industrial Register')
-    value_reg_certificate = fields.Char('Value Added Registration Certificate')
-    exempt_non = fields.Char('Exempt or non-exempt')
-    exp = fields.Integer('EXP industrial register')
-    file_tax = fields.Char('Tax File')
+    # commercial_register = fields.Char('Commercial Register')
+    # industrial_register = fields.Char('Industrial Register')
+    # value_reg_certificate = fields.Char('Value Added Registration Certificate')
+    # exempt_non = fields.Char('Exempt or non-exempt')
+    # exp = fields.Integer('EXP industrial register')
+    # file_tax = fields.Char('Tax File')
